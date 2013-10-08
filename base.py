9b42b09ddc94527d7b17eb23a84277257afc99e4
Luis Oishi <luis.shi@gmail.com>
2013-10-08 13:49:35 -0300

    add submit button

--- base.py
+++ base.py
@@ -13,13 +13,17 @@ class HelloWorld:
         self.window.set_size_request(500,200)
         self.window.set_title("Blog")
         
-        self.exit = gtk.Button("Exit")
-        self.exit.set_tooltip_text("Quit program")
-        self.exit.connect("clicked", self.destroy)
+        self.exit_button = gtk.Button("Exit")
+        self.exit_button.set_tooltip_text("Quit program")
+        self.exit_button.connect("clicked", self.destroy)
         
-        self.clear = gtk.Button("Clear")
-        self.clear.set_tooltip_text("Clear entry")
-        self.clear.connect("clicked", self.clear_entry)
+        self.clear_button = gtk.Button("Clear")
+        self.clear_button.set_tooltip_text("Clear entry")
+        self.clear_button.connect("clicked", self.clear_entry)
+
+        self.submit_button = gtk.Button("Submit")
+        self.submit_button.set_tooltip_text("Submit")
+        self.submit_button.connect("clicked", self.submit)
         
         self.user_label = gtk.Label("Username:")
         self.mail_label = gtk.Label("Email:")
@@ -35,33 +39,40 @@ class HelloWorld:
         self.email.pack_start(self.mail_entry)
         
         self.button = gtk.HBox()
-        self.button.pack_start(self.exit)
-        self.button.pack_start(self.clear)
-        
+        self.button.pack_start(self.exit_button)
+        self.button.pack_start(self.clear_button)
+        self.button.pack_start(self.submit_button)
+
         self.vbox = gtk.VBox()
-#        self.vbox.pack_start(self.login)
-#        self.vbox.pack_start(self.entry)
         self.vbox.pack_start(self.login)
         self.vbox.pack_start(self.email)
         self.vbox.pack_start(self.button)
-        
+
         self.window.add(self.vbox)
         self.window.show_all()
         self.window.connect("destroy", self.destroy)
-
-
-#   callback
-    def destroy(self, widget, data=None):
-        print "Exiting program"
-        gtk.main_quit()
 
     def main(self):
 #        All PyGTK applications must have a gtk.main(). Control ends here
 #        and waits for an event to occur (like a key press or mouse event).
         gtk.main()
 
+#   callback
+    def destroy(self, widget, data=None):
+        print "Exiting program"
+        gtk.main_quit()
+
+
     def clear_entry(self, widget):
-        print "Clearing entry"
+        self.user_entry.set_text("")
+        self.mail_entry.set_text("")
+        print "Entry cleared"
+
+    def submit(self, widget):
+        print self.user_entry.get_text()
+        print self.mail_entry.get_text()
+        print "submit"
+
 #print __name__
 if __name__ == "__main__":
     base = HelloWorld()
