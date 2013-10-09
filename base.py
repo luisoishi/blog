676de0c9e3997722c75ff4dd2d4fba26a1741f6f
Luis Oishi <luis.shi@gmail.com>
2013-10-09 17:20:46 -0300

    Add password, password check and change window title

--- base.py
+++ base.py
@@ -11,7 +11,7 @@ class HelloWorld:
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.set_position(gtk.WIN_POS_CENTER)
         self.window.set_size_request(500,200)
-        self.window.set_title("Blog")
+        self.window.set_title("Register")
         
         self.exit_button = gtk.Button("Exit")
         self.exit_button.set_tooltip_text("Quit program")
@@ -27,8 +27,14 @@ class HelloWorld:
         
         self.user_label = gtk.Label("Username:")
         self.mail_label = gtk.Label("Email:")
+        self.password_label = gtk.Label("Password:")
+        self.password_check_label = gtk.Label("Re-type password")
         self.user_entry = gtk.Entry()
         self.mail_entry = gtk.Entry()
+        self.password_entry = gtk.Entry()
+        self.password_entry.set_visibility(gtk.FALSE)
+        self.password_check_entry = gtk.Entry()
+        self.password_check_entry.set_visibility(gtk.FALSE)
         
         self.login = gtk.HBox()
         self.login.pack_start(self.user_label)
@@ -38,6 +44,14 @@ class HelloWorld:
         self.email.pack_start(self.mail_label)
         self.email.pack_start(self.mail_entry)
         
+        self.password = gtk.HBox()
+        self.password.pack_start(self.password_label)
+        self.password.pack_start(self.password_entry)
+
+        self.password_check = gtk.HBox()
+        self.password_check.pack_start(self.password_check_label)
+        self.password_check.pack_start(self.password_check_entry)
+
         self.button = gtk.HBox()
         self.button.pack_start(self.exit_button)
         self.button.pack_start(self.clear_button)
@@ -46,6 +60,8 @@ class HelloWorld:
         self.vbox = gtk.VBox()
         self.vbox.pack_start(self.login)
         self.vbox.pack_start(self.email)
+        self.vbox.pack_start(self.password)
+        self.vbox.pack_start(self.password_check)
         self.vbox.pack_start(self.button)
 
         self.window.add(self.vbox)
@@ -66,11 +82,15 @@ class HelloWorld:
     def clear_entry(self, widget):
         self.user_entry.set_text("")
         self.mail_entry.set_text("")
+        self.password_entry.set_text("")
+        self.password_check_entry.set_text("")
         print "Entry cleared"
 
     def submit(self, widget):
         print self.user_entry.get_text()
         print self.mail_entry.get_text()
+        print self.password_entry.get_text()
+        print self.password_check_entry.get_text()
         print "submit"
 
 #print __name__
